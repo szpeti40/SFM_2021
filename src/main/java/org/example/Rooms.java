c3f0f40884b647ee842b7bd8b936f844372bda1d
Szilagyi <sz.joci12@gmail.com>
2021-04-14 22:02:15 +0200

    basic project added

---
+++ src/main/java/org/example/Rooms.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Rooms {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String bedType;
+    private boolean balcony;
+    private Integer receptionist;
+    private Integer price;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBedType() {
+        return bedType;
+    }
+
+    public void setBedType(String bedType) {
+        this.bedType = bedType;
+    }
+
+    public boolean isBalcony() {
+        return balcony;
+    }
+
+    public void setBalcony(boolean balcony) {
+        this.balcony = balcony;
+    }
+
+    public Integer getReceptionist() {
+        return receptionist;
+    }
+
+    public void setReceptionist(Integer receptionist) {
+        this.receptionist = receptionist;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
